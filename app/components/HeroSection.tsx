f6bb0ae24a79063b57240d5db413c9919cf0599c
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-10-08 18:45:56 +0600

    Fix hero image height issue bug

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -19,25 +19,26 @@ const itemVariants = {
   visible: { opacity: 1, y: 0 },  
 };
 export const HeroSection = () => {
+   
 
 
   return (
-    <motion.div className="flex flex-col lg:flex-row justify-between items-center min-h-[80vh]"
+    <motion.div className="flex flex-col lg:flex-row justify-between items-center max-w-6xl mx-auto px-6"
     variants={containerVariants}
     initial="hidden"
     animate="visible"
     >
-      <div className="flex-1 max-w-lg mb-10 lg:mb-0">
+      <div className="flex-1 max-w-xl mb-10 lg:mb-0">
         <div className="flex flex-col gap-5">
           <motion.p className="text-xl font-bold text-[var(--color-accent-secondary)]"
           variants={itemVariants}
           >Hello!</motion.p>
           <motion.p className="text-3xl font-bold"
           variants={itemVariants}
-          > I'm Professional Fullstack Developer &  
-            <span className="text-[var(--color-accent-secondary)]"> Aspiring Software Engineer</span></motion.p>
+          > I'm Professional Fullstack Developer &   
+          <span className="text-[var(--color-accent-secondary)]"> Software Engineer</span></motion.p>
           <motion.p variants={itemVariants}>
-            I specialize in building robust and scalable applications using modern web technologies — from React and Next.js to Node.js, Express, and ASP.NET Core. I focus on creating clean, efficient, and user-centered solutions across the full stack.
+            I specialize in building robust and scalable applications using modern web technologies — from React and Next.js to Node.js, Express, and ASP.NET Core.
           </motion.p>
         </div>
         <motion.div className="flex mt-5 gap-6" variants={itemVariants}>
@@ -49,7 +50,7 @@ export const HeroSection = () => {
           >Download CV</AnimatedButton>
         </motion.div>
       </div>
-      <motion.div className="flex-1 w-full aspect-[3/4] lg:aspect-auto lg:h-[100vh] relative max-w-md"
+      <motion.div className="flex-1 w-full aspect-[3/4] lg:aspect-[1/1] relative max-w-md lg:h-[650px]"
       initial={{ opacity: 0, x: 50 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ delay: 0.5, duration: 0.7 }}
