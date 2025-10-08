5bd4785e8e021b7a89d1fe508ae5b388fa97af2d
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-10-08 12:49:38 +0600

    Implement Framer Motion staggering

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,31 +1,62 @@
+"use client";
 import Image from "next/image";
+import { motion } from "framer-motion";
 import { AnimatedButton } from "./AnimatedButton";
+
+  const containerVariants = {
+  hidden: { opacity: 0 }, 
+  visible: {
+    opacity: 1,          
+    transition: {
+      staggerChildren: 0.15, 
+      delayChildren: 0.3,   
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 30 }, 
+  visible: { opacity: 1, y: 0 },  
+};
 export const HeroSection = () => {
 
+
   return (
-    <div className="flex justify-between items-center min-h-[80vh]">
+    <motion.div className="flex justify-between items-center min-h-[80vh]"
+    variants={containerVariants}
+    initial="hidden"
+    animate="visible"
+    >
       <div className="flex-1 max-w-lg">
         <div className="flex flex-col gap-5">
-          <p className="text-xl font-bold text-[var(--color-accent-secondary)]">Hello!</p>
-          <p className="text-3xl font-bold"> I'm Professional Web Designer &  
-            <span className="text-[var(--color-accent-secondary)]"> Wordpress Developer</span></p>
-          <p>
+          <motion.p className="text-xl font-bold text-[var(--color-accent-secondary)]"
+          variants={itemVariants}
+          >Hello!</motion.p>
+          <motion.p className="text-3xl font-bold"
+          variants={itemVariants}
+          > I'm Professional Web Designer &  
+            <span className="text-[var(--color-accent-secondary)]"> Wordpress Developer</span></motion.p>
+          <motion.p variants={itemVariants}>
             It is a long established fact that a reader will be distracted by
             the readable content of a page when looking at its layout.
-          </p>
+          </motion.p>
         </div>
-        <div className="flex mt-5 gap-6">
+        <motion.div className="flex mt-5 gap-6" variants={itemVariants}>
           <AnimatedButton
           variant="primary"
           >Hire Me</AnimatedButton>
           <AnimatedButton
           variant="secondary"
           >Download CV</AnimatedButton>
-        </div>
+        </motion.div>
       </div>
-      <div className="flex-1 h-[100vh] relative max-w-md">
+      <motion.div className="flex-1 h-[100vh] relative max-w-md"
+      initial={{ opacity: 0, x: 50 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ delay: 0.5, duration: 0.7 }}
+      >
         <Image src="/hero.png" alt={"hero"} fill className="object-cover object-center" />
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
