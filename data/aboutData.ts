d886489809e9a5aa4aa3831a29beef8fb841113b
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-10-08 21:59:37 +0600

    Add about me data

---
+++ data/aboutData.ts
@@ -0,0 +1,14 @@
+import { Github, Linkedin, Mail } from "lucide-react";
+
+export const stats = [
+  { value: "6+", label: "Key Technologies" },
+  { value: "4", label: "Projects Deployed" },
+  { value: "1230+", label: "Code Commits" },
+  { value: "1.5", label: "Years of Experience" },
+];
+
+export const socialLinks = [
+  { icon: Github, href: "https://github.com/sumiya-yasmin" },
+  { icon: Linkedin, href: "https://www.linkedin.com/in/sumiya-yasmin/" },
+  { icon: Mail, href: "mailto:rahmansimi3996@gmail.com" },
+];
