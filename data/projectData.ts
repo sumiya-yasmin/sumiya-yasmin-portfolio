46b7c34f56c7ab25bdebef420d4a6ae98380f084
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-10-09 12:29:52 +0600

    Add project mock data

---
+++ data/projectData.ts
@@ -0,0 +1,36 @@
+export const categories = ["ALL", "FULLSTACK", "NEXT.JS", "API/NODE"];
+
+export const projectItems = [
+    { 
+        id: 1, 
+        image: "/linkdeen.png", 
+        title: "Social Media Platform", 
+        categories: ["FULLSTACK", "WEB-APP", "MERN"], 
+        liveLink: "https://link-deen.vercel.app/", 
+        githubLink: "https://github.com/sumiya-yasmin/link-deen" 
+    },
+    { 
+        id: 2, 
+        image: "/item3.png",
+        title: "Inventory App", 
+        categories: ["FULLSTACK","WEB-APP", "ASP-DOTNET-CORE", "API/NODE"], 
+        liveLink: "https://github.com/sumiya-yasmin/InventoryApp",
+        githubLink: "https://github.com/sumiya-yasmin/InventoryApp"
+    },
+       { 
+        id: 3, 
+        image: "/item1.png",
+        title: "EXpense Income Tracker", 
+        categories: ["FRONTEND","MOBILE-APP", "REACT-NATIVE"], 
+        liveLink: "https://github.com/sumiya-yasmin/expense-income-tracker-react-native-ui",
+        githubLink: "https://github.com/sumiya-yasmin/expense-income-tracker-react-native-ui"
+    },
+    { 
+        id: 4, 
+        image: "/item2.png",
+        title: "Appointment Booking App", 
+        categories: ["FULLSTACK", "WEB-APP", "MERN"], 
+        liveLink: "https://github.com/bongodev/appointment-booking",
+        githubLink: "https://github.com/bongodev/appointment-booking"
+    },
+];
