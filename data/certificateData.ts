95255bfd123e53c6461ee9f510e6b9f10d77fe6c
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-10-10 14:57:43 +0600

    Add certificate Data

---
+++ data/certificateData.ts
@@ -0,0 +1,21 @@
+import { CertificateData } from "@/app/types";
+
+export const CERTIFICATE_DATA: CertificateData[] =[ {
+    tag: "VERIFIED",
+    date: "1st July, 2025",
+    title: "Data Analysis Live Course",
+    subtitle: "By 10 Minute School (SheSTEM Initiative)",
+    description: "Completed comprehensive training and examination in data analysis techniques, covering core statistical concepts and practical application.",
+    link: "https://drive.google.com/file/d/176Mf7G39Z3-kn4_yYWECIsM2zCbpSzJ9/view",
+    imageUrl: "/C4.png", 
+},
+// {
+//     tag: "VERIFIED",
+//     date: "1st July, 2025",
+//     title: "Data Analysis Live Course",
+//     subtitle: "By 10 Minute School (SheSTEM Initiative)",
+//     description: "Completed comprehensive training and examination in data analysis techniques, covering core statistical concepts and practical application.",
+//     link: "https://drive.google.com/file/d/176Mf7G39Z3-kn4_yYWECIsM2zCbpSzJ9/view",
+//     imageUrl: "/C4.png", 
+// },
+];
