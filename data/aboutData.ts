81e4e7c2c07b8a10468f9d7df50f111d0ee33cf2
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-10-10 17:21:13 +0600

    Update about data

--- data/aboutData.ts
+++ data/aboutData.ts
@@ -1,10 +1,10 @@
 import { Github, Linkedin, Mail } from "lucide-react";
 
 export const stats = [
-  { value: "6+", label: "Key Technologies" },
-  { value: "4", label: "Projects Deployed" },
-  { value: "1230+", label: "Code Commits" },
-  { value: "1.5", label: "Years of Experience" },
+  { value: 6, suffix: "+", label: "Key Technologies" },
+  { value: 4, label: "Projects Deployed" },
+  { value: 1230, suffix: "+",  label: "Code Commits" },
+  { value: 1.5, decimals: 1, label: "Years of Experience" },
 ];
 
 export const socialLinks = [
