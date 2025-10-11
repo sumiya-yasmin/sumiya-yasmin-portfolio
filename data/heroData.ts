716fa44ec0ff542a5d34bf06ea21c0f3f4c97153
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-10-11 23:40:57 +0600

    Rename heroData and update stat data with stat links

---
+++ data/heroData.ts
@@ -0,0 +1,14 @@
+import { Github, Linkedin, Mail } from "lucide-react";
+
+export const stats = [
+  { value: 6, suffix: "+", label: "Key Technologies", link: "#skills" },
+  { value: 4, label: "Projects Deployed", link: "#projects" },
+  { value: 1230, suffix: "+",  label: "Code Commits", link: "https://github.com/sumiya-yasmin?tab=overview&from=2025-10-01&to=2025-10-10" },
+  { value: 1.5, decimals: 1, label: "Years of Experience", link: "#experience" },
+];
+
+export const socialLinks = [
+  { icon: Github, href: "https://github.com/sumiya-yasmin" },
+  { icon: Linkedin, href: "https://www.linkedin.com/in/sumiya-yasmin/" },
+  { icon: Mail, href: "mailto:rahmansimi3996@gmail.com" },
+];
